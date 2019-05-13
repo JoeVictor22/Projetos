b5d69d13db28e7345aa851cc129aa7bfe4f20eb0
Joel <JoelVIctor1746@gmail.com>
2019-05-13 12:52:11 -0300

    Update stringTest.c

--- Huffman_Compression/temp/stringTest.c
+++ Huffman_Compression/temp/stringTest.c
@@ -18,15 +18,22 @@ int main(){
 	test->caminho[2] = '\0';
 	test->caminho[3] = 'a';
 		
+	printf("strlen : %d\n", strlen(test->caminho));
+	test->caminho = realloc(test->caminho, (char*)((strlen(test->caminho) + 2 ) * sizeof(char)));
+	
 
 	printf("%s\n", test->caminho);
 	printf("%c\n", test->caminho[0]);
 
 	
 
-	printf("eoq :%d\n", strlen(test->caminho));	
-	
 	strcat(test->caminho, "0");
+
+	test->caminho[3] = '\0';
+
+	
+	printf("STRLEN 2 :%d\n", strlen(test->caminho));	
+	
 
 	printf("%s\n", test->caminho);
 	printf("%c\n", *test->caminho);
