d583d88900b9dd3925781b2b5881ba8a0b9a6800
Joel <JoelVIctor1746@gmail.com>
2019-09-11 11:08:31 -0300

    Create install.sh

---
+++ SeleniumTests/installation&test/install.sh
@@ -0,0 +1,10 @@
+#instala selenium
+pip install selenium
+#faz o download do arquivo
+wget https://github.com/mozilla/geckodriver/releases/download/v0.25.0/geckodriver-v0.25.0-linux32.tar.gz
+#extrai o download
+tar -xvzf geckodriver-v0.25.0-linux32.tar.gz geckodriver
+#torna o bin executavel
+chmod +x geckodriver
+#move pra o diretorio
+sudo mv geckodriver /usr/local/bin/
